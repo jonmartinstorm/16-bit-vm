eb37f772fcb2b467893cc803cca43357661371e6
Jon-Martin Storm <jonmartin.storm@gmail.com>
2021-02-22 11:55:34 +0100

    episode-1 done

--- node-vm/cpu.js
+++ node-vm/cpu.js
@@ -1,4 +1,5 @@
 const createMemory = require("./memory");
+const instructions = require("./instructions");
 
 class CPU {
     constructor(memory) {
@@ -19,15 +20,22 @@ class CPU {
         }, {});
     }
 
+    debug() {
+        this.registerNames.forEach(name => {
+            console.log(`${name}: 0x${this.getRegister(name).toString(16).padStart(4, "0")}`)
+        });
+        console.log();
+    }
+
     getRegister(name) {
-        if (!(name in this.registerNames)) {
+        if (!(name in this.registerMap)) {
             throw new Error(`getRegister: no such register '${name}'`);
         }
         return this.registers.getUint16(this.registerMap[name]);
     }
 
     setRegister(name, value) {
-        if (!(name in this.registerNames)) {
+        if (!(name in this.registerMap)) {
             throw new Error(`setRegister: no such register '${name}'`);
         }
         return this.registers.setUint16(this.registerMap[name], value);
@@ -40,10 +48,41 @@ class CPU {
         return instruction;
     }
 
+    fetch16() {
+        const nextInstructonAddress = this.getRegister("ip");
+        const instruction = this.memory.getUint16(nextInstructonAddress);
+        this.setRegister('ip', nextInstructonAddress + 2);
+        return instruction;
+    }
+
     execute(instruction) {
         switch (instruction) {
+            case instructions.MOV_LIT_R1: {
+                const literal = this.fetch16();
+                this.setRegister("r1", literal);
+                return;
+            }
+            
+            case instructions.MOV_LIT_R2: {
+                const literal = this.fetch16();
+                this.setRegister("r2", literal);
+                return;
+            }
 
+            case instructions.ADD_REG_REG: {
+                const r1 = this.fetch();
+                const r2 = this.fetch();
+                const registerValue1 = this.registers.getUint16(r1 * 2);
+                const registerValue2 = this.registers.getUint16(r2 * 2);
+                this.setRegister("acc", registerValue1 + registerValue2);
+                return;
+            }
         }
+    }
+
+    step() {
+        const instruction = this.fetch();
+        return this.execute(instruction);
     }
 }
 
