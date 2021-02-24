57cde828c42c083ca73ecdb854d1aba50b539bc8
Jon-Martin Storm <jonmartin.storm@gmail.com>
2021-02-24 10:35:43 +0100

    more rusty iterator

--- rust-vm/src/main.rs
+++ rust-vm/src/main.rs
@@ -4,17 +4,27 @@ mod memory;
 
 use std::io;
 
+use std::iter;
+
+
+
+
+
 use crate::memory::Memory;
 use crate::cpu::CPU;
 
 fn main() {
     let mut m = Memory::new(256*256);
-    let mut i = 0;
+
+    // Infinte iterator that increases 1 every time it is called
+    let mut curr = 0;
+    let mut i = iter::repeat_with(|| { let tmp = curr; curr += 1; tmp });
 
     let ip  = 0;
     let acc = 1;
     let r1  = 2;
     let r2  = 3;
+
 
     // start:
     //   mov #0x0100, r1
@@ -23,49 +33,30 @@ fn main() {
     //   mov acc, #0x0100
     //   jne 0x0003, start:
 
-    m.set_uint8(i, instructions::MOV_MEM_REG);
-    i += 1;
-    m.set_uint8(i, 0x01);
-    i += 1;
-    m.set_uint8(i, 0x00);
-    i += 1;
-    m.set_uint8(i, r1);
-    i += 1;
+    m.set_uint8(i.next().unwrap(), instructions::MOV_MEM_REG);
+    m.set_uint8(i.next().unwrap(), 0x01);
+    m.set_uint8(i.next().unwrap(), 0x00);
+    m.set_uint8(i.next().unwrap(), r1);
 
-    m.set_uint8(i, instructions::MOV_LIT_REG);
-    i += 1;
-    m.set_uint8(i, 0x00);
-    i += 1;
-    m.set_uint8(i, 0x01);
-    i += 1;
-    m.set_uint8(i, r2);
-    i += 1;
+    m.set_uint8(i.next().unwrap(), instructions::MOV_LIT_REG);
+    m.set_uint8(i.next().unwrap(), 0x00);
+    m.set_uint8(i.next().unwrap(), 0x01);
+    m.set_uint8(i.next().unwrap(), r2);
 
-    m.set_uint8(i, instructions::ADD_REG_REG);
-    i += 1;
-    m.set_uint8(i, r1);
-    i += 1;
-    m.set_uint8(i, r2);
-    i += 1;
+    m.set_uint8(i.next().unwrap(), instructions::ADD_REG_REG);
+    m.set_uint8(i.next().unwrap(), r1);
+    m.set_uint8(i.next().unwrap(), r2);
 
-    m.set_uint8(i, instructions::MOV_REG_MEM);
-    i += 1;
-    m.set_uint8(i, acc);
-    i += 1;
-    m.set_uint8(i, 0x01);
-    i += 1;
-    m.set_uint8(i, 0x00);
-    i += 1;
+    m.set_uint8(i.next().unwrap(), instructions::MOV_REG_MEM); 
+    m.set_uint8(i.next().unwrap(), acc);
+    m.set_uint8(i.next().unwrap(), 0x01);
+    m.set_uint8(i.next().unwrap(), 0x00);
 
-    m.set_uint8(i, instructions::JMP_NOT_EQ);
-    i += 1;
-    m.set_uint8(i, 0x00);
-    i += 1;
-    m.set_uint8(i, 0x03);
-    i += 1;
-    m.set_uint8(i, 0x00); // 0x0000 start:
-    i += 1;
-    m.set_uint8(i, 0x00);
+    m.set_uint8(i.next().unwrap(), instructions::JMP_NOT_EQ);
+    m.set_uint8(i.next().unwrap(), 0x00);
+    m.set_uint8(i.next().unwrap(), 0x03);
+    m.set_uint8(i.next().unwrap(), 0x00); // 0x0000 start:
+    m.set_uint8(i.next().unwrap(), 0x00);
   
     let mut cpu = CPU::new(m);
 
