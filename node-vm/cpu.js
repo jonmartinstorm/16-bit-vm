b8850be81535008dab336ba155dce191e350da98
Jon-Martin Storm <jonmartin.storm@gmail.com>
2021-02-24 09:10:51 +0100

    node-vm done

--- node-vm/cpu.js
+++ node-vm/cpu.js
@@ -27,6 +27,14 @@ class CPU {
         console.log();
     }
 
+    viewMemoryAt(address) {
+        const nextEightBytes = Array.from({length: 8}, (_, i) => 
+            this.memory.getUint8(address + i)
+        ).map(v => `0x${v.toString(16).padStart(2, "0")}`);
+
+        console.log(`${address.toString(16).padStart(4, "0")}: ${nextEightBytes.join(" ")}`);
+    }
+
     getRegister(name) {
         if (!(name in this.registerMap)) {
             throw new Error(`getRegister: no such register '${name}'`);
@@ -57,9 +65,34 @@ class CPU {
 
     execute(instruction) {
         switch (instruction) {
-            case instructions.MOV_LIT_R1: {
+            case instructions.MOV_LIT_REG: {
                 const literal = this.fetch16();
-                this.setRegister("r1", literal);
+                const register = (this.fetch() % this.registerNames.length) * 2;
+                this.registers.setUint16(register, literal);
+                return;
+            }
+
+            case instructions.MOV_REG_REG: {
+                const registerFrom = (this.fetch() % this.registerNames.length) * 2;
+                const registerTo = (this.fetch() % this.registerNames.length) * 2;
+                const value = this.registers.getUint16(registerFrom);
+                this.registers.setUint16(registerTo, value);
+                return;
+            }
+
+            case instructions.MOV_REG_MEM: {
+                const registerFrom = (this.fetch() % this.registerNames.length) * 2;
+                const address = this.fetch16();
+                const value = this.registers.getUint16(registerFrom);
+                this.memory.setUint16(address, value);
+                return;
+            }
+
+            case instructions.MOV_MEM_REG: {
+                const address = this.fetch16();
+                const registerTo = (this.fetch() % this.registerNames.length) * 2;
+                const value = this.memory.getUint16(address);
+                this.registers.setUint16(registerTo, value);
                 return;
             }
             
@@ -75,6 +108,17 @@ class CPU {
                 const registerValue1 = this.registers.getUint16(r1 * 2);
                 const registerValue2 = this.registers.getUint16(r2 * 2);
                 this.setRegister("acc", registerValue1 + registerValue2);
+                return;
+            }
+
+            case instructions.JMP_NOT_EQ: {
+                const literal = this.fetch16();
+                const address = this.fetch16();
+
+                if (literal !== this.getRegister("acc")) {
+                    this.setRegister("ip", address);
+                }
+
                 return;
             }
         }
--- node-vm/index.js
+++ node-vm/index.js
@@ -1,33 +1,65 @@
+const readline = require("readline");
 const createMemory = require("./memory");
 const CPU = require("./cpu");
 const instructions = require("./instructions");
 
-const memory = createMemory(256);
+const IP  = 0;
+const ACC = 1;
+const R1  = 2;
+const R2  = 3;
+
+const memory = createMemory(256*256);
 const writeableBytes = new Uint8Array(memory.buffer);
 
 const cpu = new CPU(memory);
 
 let i = 0;
 
-writeableBytes[i++] = instructions.MOV_LIT_R1;
-writeableBytes[i++] = 0x12;
-writeableBytes[i++] = 0x34;
+// start:
+//   mov #0x0100, r1
+//   mov  0x0001, r2
+//   add r1, r2
+//   mov acc, #0x0100
+//   jne 0x0003, start:
 
-writeableBytes[i++] = instructions.MOV_LIT_R2;
-writeableBytes[i++] = 0xAB;
-writeableBytes[i++] = 0xCD;
+writeableBytes[i++] = instructions.MOV_MEM_REG;
+writeableBytes[i++] = 0x01; // 0x0100
+writeableBytes[i++] = 0x00;
+writeableBytes[i++] = R1;
+
+writeableBytes[i++] = instructions.MOV_LIT_REG;
+writeableBytes[i++] = 0x00;
+writeableBytes[i++] = 0x01;
+writeableBytes[i++] = R2;
 
 writeableBytes[i++] = instructions.ADD_REG_REG;
-writeableBytes[i++] = 0x02; // r1
-writeableBytes[i++] = 0x03; // r2
+writeableBytes[i++] = R1;
+writeableBytes[i++] = R2; 
 
-cpu.debug();
+writeableBytes[i++] = instructions.MOV_REG_MEM;
+writeableBytes[i++] = ACC;
+writeableBytes[i++] = 0x01; // 0x0100
+writeableBytes[i++] = 0x00;
+
+writeableBytes[i++] = instructions.JMP_NOT_EQ;
+writeableBytes[i++] = 0x00;
+writeableBytes[i++] = 0x03;
+writeableBytes[i++] = 0x00; // 0x0000 start:
+writeableBytes[i++] = 0x00;
 
 cpu.step();
 cpu.debug();
+cpu.viewMemoryAt(cpu.getRegister("ip"));
+cpu.viewMemoryAt(0x0100);
 
-cpu.step();
-cpu.debug();
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-cpu.step();
-cpu.debug();
+rl.on("line", () => {
+    cpu.step();
+    cpu.debug();
+    cpu.viewMemoryAt(cpu.getRegister("ip"));
+    cpu.viewMemoryAt(0x0100);
+});
